f4f456b159f65bbebcd0c3806327cd4a1a1e91be
Gustavo Martinez <gustavopalmr@gmail.com>
2024-07-17 14:57:31 -0300

    Commit da aula de header

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from recipes.views import home, contato, sobre
+
+from recipes.views import home 
      
 urlpatterns = [
-    path('sobre/', sobre),
-    path('contato/', contato),
     path('', home),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html', context = {'name': 'Gustavo Martinez'})
 
-def contato(request):
-    return HttpResponse('temp/temp.html')
-
-def sobre(request):
-    return HttpResponse('sobre')
