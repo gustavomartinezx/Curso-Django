b5782f6a76ef594c09d052c2af4fd2f73ac5fb9e
Gustavo Martinez <gustavopalmr@gmail.com>
2024-09-26 15:19:26 -0300

    ⬆️  adicionando controle de faixas estáticas, e a rota da receita

--- recipes/views.py
+++ recipes/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'recipes/pages/home.html', context = {'name': 'Gustavo Martinez'})
 
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context = {
+        'name': 'Gustavo Martinez'
+        })
