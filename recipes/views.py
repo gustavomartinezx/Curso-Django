168438bac023bb9485f4da3ac1938723007b81ed
gustavomartinezx <gustavopalmr@gmail.com>
2024-10-29 16:20:01 -0300

    ✨🔧 Ajusta title da página de categoria e adiciona resposta 404

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 from utils.recipes.factory import make_recipe
 from .models import Recipe
 
@@ -12,8 +13,13 @@ def category(request, category_id):
     recipes = Recipe.objects.filter(
         category__id=category_id, is_published=True
         ).order_by('-id')
+    
+    if not recipes:
+        raise Http404('Not found 😫')
+
     return render(request, 'recipes/pages/category.html', context = {
-        'recipes': recipes
+        'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category | ',
         })
 
 def recipe(request, id):
